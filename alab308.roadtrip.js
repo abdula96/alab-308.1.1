eb586b7f28b670ebd179c7be9213fadec9408dfb
abdula96 <ahmed.abdula.hslc@gmail.com>
2024-10-22 11:35:27 -0400

    constants

---
+++ alab308.roadtrip.js
@@ -0,0 +1,11 @@
+// Constants
+const totalDistance = 1500; // Total distance of the trip in miles
+const fuelCostPerGallon = 3; // Cost of fuel per gallon in dollars
+const fuelBudget = 175; // Total fuel budget in dollars
+
+// Fuel efficiency at different speeds (miles per gallon)
+const fuelEfficiency = {
+    "55mph": 30,
+    "60mph": 28,
+    "75mph": 23
+};
